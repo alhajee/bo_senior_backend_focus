a53a9189512832028539d1e4bc23066441199ab5
Muhammad Alhajee <alhajee2009@gmail.com>
2022-01-04 17:14:19 +0100

    create serializer views

--- app/repertoire/views.py
+++ app/repertoire/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from .models import File, Work
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import FileSerializer, WorkSerializer
+
+
+class FileViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows files to be viewed or edited.
+    """
+    queryset = File.objects.all().order_by('-created_at')
+    serializer_class = FileSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+
+
+class WorkViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows musical works to be viewed or edited.
+    """
+    queryset = Work.objects.all()
+    serializer_class = WorkSerializer
+    # permission_classes = [permissions.IsAuthenticated]
