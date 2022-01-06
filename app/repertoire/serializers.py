4a19e46268c7188dccccb11597ecbdeefc7aecda
Muhammad Alhajee <alhajee2009@gmail.com>
2022-01-06 09:26:04 +0100

    fix file serializer

--- app/repertoire/serializers.py
+++ app/repertoire/serializers.py
@@ -1,14 +1,31 @@
 from .models import File, Contributor, Work
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 
-class FileSerializer(serializers.HyperlinkedModelSerializer):
+class ContributorSerializer(serializers.ModelSerializer):
     class Meta:
-        model = File
-        fields = ['filename', 'work_count', 'created_at']
+        model = Contributor
+        fields = ['name']
 
 
-class WorkSerializer(serializers.HyperlinkedModelSerializer):
+class WorkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Work
-        fields = ['iswc', 'title', 'contributors', 'source', 'proprietary_id']
+        fields = ['id', 'proprietary_id', 'iswc', 'source', 'title', 'contributors']
+
+
+class FileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = File
+        fields = ['id', 'filename', 'work_count']
+
+
+class WorksSerializer(serializers.Serializer):
+    class Meta:
+        works = WorkSerializer(many=True)
+        files = FileSerializer(many=True)
+        count = 1
+        fields = ['count', 'files', 'works']
+
+
