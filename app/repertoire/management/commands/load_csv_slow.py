4fb3e5524c71d15eb630953b40080e140bc9e939
Muhammad Alhajee <alhajee2009@gmail.com>
2022-01-08 03:03:58 +0100

    Implement a naive way to load csv data

---
+++ app/repertoire/management/commands/load_csv_slow.py
@@ -0,0 +1,115 @@
+import csv
+from django.core.management.base import BaseCommand, CommandError
+from django.utils import timezone
+
+from repertoire.models import Contributor, File, Work
+
+
+class Command(BaseCommand):
+    help = "Loads musical works from CSV file and saves them in database"
+
+    def add_arguments(self, parser):
+        parser.add_argument("file_path", type=str)
+
+        # # Named (optional) arguments
+        parser.add_argument(
+            '--header',
+            help='Does csv file has headers',
+            type=bool
+        )
+
+    def handle(self, *args, **options):
+        # clean database
+        Contributor.objects.all().delete()
+        Work.objects.all().delete()
+        File.objects.all().delete()
+
+        start_time = timezone.now()
+        file_path = options["file_path"]
+        
+        header = True
+
+        # check if optional header argument was provided
+        if options['header']:
+            header = options['header']
+
+        try:
+            with open(f"/files/{file_path}", "r") as csv_file:
+                # does csv file has header
+
+                # get start time
+                start_time = timezone.now()
+                
+                # define the max rows to insert into the database
+                MAX_ROWS = 1000
+
+                # define default index of columns
+                columns = {
+                    "title": 0,
+                    "contributors": 1,
+                    "iswc": 2,
+                    "source": 3,
+                    "proprietary_id": 4
+                }
+
+                # create a data reader
+                csv_reader = csv.reader(csv_file, delimiter=",")
+
+                # # get number of rows in file
+                # row_count = sum(1 for row in csv_reader)
+                row_count = 0
+                
+                header_list = list()
+
+                # update column index if headers do not come in expected order
+                if header is True:
+                    header_list = next(csv_reader)
+
+                    for index, column_name in enumerate(header_list):
+                        columns[column_name] = index
+                
+                # save file metadata in model
+                file = File(
+                    filename = file_path,
+                    work_count = row_count
+                )
+                file.save()
+            
+                # read works and save them in batches into database
+                for row in csv_reader:
+                    # get the various name of contributors
+                    contributor_list = row[columns.get("contributors")]
+                    contributor_list = contributor_list.split("|")
+
+                    # create object from metadata on a musical work
+                    work = Work(
+                        file=file,
+                        title=row[columns.get("title")],
+                        iswc=row[columns.get("iswc")],
+                        source=row[columns.get("source")],
+                        proprietary_id=row[columns.get("proprietary_id")]
+                    )
+                    work.save()
+                    # save contributors or get ID if it exist in the database
+                    for contributor in contributor_list:
+                        obj, created = Contributor.objects.get_or_create(
+                            name=contributor
+                        )
+                        # save if object was created not retrieved
+                        if created:
+                            # print(f"created {created}")
+                            obj.save()
+                        work.contributors.add(obj)
+
+                end_time = timezone.now()
+   
+        except FileNotFoundError as e:
+            raise FileNotFoundError('File "%s" does not exist' % file_path)
+        except Exception as e:
+            print(e)
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Loading CSV took: {(end_time-start_time).total_seconds()} seconds."
+            )
+        )
