019ed764f711b5d3992574d62e181be945932be9
Muhammad Alhajee <alhajee2009@gmail.com>
2022-01-09 05:08:34 +0100

    write tests for File model

--- app/repertoire/tests.py
+++ app/repertoire/tests.py
@@ -1,3 +1,135 @@
-from django.test import TestCase
+from io import StringIO
+import json
 
-# Create your tests here.
+from django.core.management import call_command
+from django.core.management.base import CommandError
+from django.db.utils import IntegrityError
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from repertoire.serializers import (
+    Contributor, 
+    File, 
+    Work
+)
+
+
+class FileTestCase(APITestCase):
+    expected_response = dict()
+
+    def setUp(self):
+        self.file_1 = File.objects.create(
+            filename = "sony.csv",
+            work_count = 2
+        )
+        self.file_2 = File.objects.create(
+            filename = "universal.csv",
+            work_count = 1
+        )
+        self.file_3 = File.objects.create(
+            filename = "warner.csv",
+            work_count = 3
+        )
+
+        self.expected_response['files'] = [
+            {'id': self.file_1.id, 'filename': 'sony.csv', 'work_count': 2},
+            {'id': self.file_2.id, 'filename': 'universal.csv', 'work_count': 1}, 
+            {'id': self.file_3.id, 'filename': 'warner.csv', 'work_count': 3}
+        ]
+
+        self.expected_response['file_1_response'] = self.expected_response['files'][0]
+        self.expected_response['file_2_response'] = self.expected_response['files'][1]
+        self.expected_response['file_3_response'] = self.expected_response['files'][2]
+
+        self.expected_response['not_found'] = {
+            "detail": "Not found."
+        }
+
+
+    def test_list_files(self):
+        response = self.client.get("/files/")
+        
+        expected_response = self.expected_response['files']
+        request_response = json.loads(response.content)
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+        self.assertEqual(
+            request_response,
+            expected_response
+        )
+
+        
+    def test_get_file_1_ok(self):
+        response = self.client.get(f"/files/{self.file_1.id}/")
+        
+        expected_response = self.expected_response['file_1_response']
+        request_response = json.loads(response.content)
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+        self.assertEqual(
+            request_response,
+            expected_response
+        )
+
+
+    def test_get_file_2_ok(self):
+        response = self.client.get(f"/files/{self.file_2.id}/")
+        
+        expected_response = self.expected_response['file_2_response']
+        request_response = json.loads(response.content)
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+        self.assertEqual(
+            request_response,
+            expected_response
+        )
+
+
+    def test_get_file_3_ok(self):
+        response = self.client.get(f"/files/{self.file_3.id}/")
+        
+        expected_response = self.expected_response['file_3_response']
+        request_response = json.loads(response.content)
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+        self.assertEqual(
+            request_response,
+            expected_response
+        )
+
+
+    def test_get_file_not_found(self):
+        response = self.client.get(f"/files/4/")
+        
+        expected_response = self.expected_response['not_found']
+        request_response = json.loads(response.content)
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_404_NOT_FOUND
+        )
+
+        self.assertEqual(
+            request_response,
+            expected_response
+        )
+
+
